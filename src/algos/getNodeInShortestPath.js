93fa52f05591ff0f516712fa598e955c411a598a
kartik298 <56475405+kartik298@users.noreply.github.com>
2022-07-10 23:33:24 +0530

    Delete getNodeInShortestPath.js

--- src/algos/getNodeInShortestPath.js
+++
@@ -1,9 +0,0 @@
-export function getNodesInShortestPathOrder(finishNode) {
-    const nodesInShortestPathOrder = [];
-    let currentNode = finishNode;
-    while (currentNode !== null) {
-      nodesInShortestPathOrder.unshift(currentNode);
-      currentNode = currentNode.prev;
-    }
-    return nodesInShortestPathOrder;
-  }
